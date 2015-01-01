f7abd6de17b9be42288f5677180146eccea09d64
HarHar <harhar-captain@live.com>
2015-01-01 12:54:11 -0200

    Replace threads with generators to merge both stdout and stderr

--- gee.py
+++ gee.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 """
@@ -18,10 +18,36 @@
 
 import subprocess
 import threading
+from Queue import Queue, Empty
 from moviepy.editor import *
 import os, sys
 import time
 key = "'" #key is '
+
+class NonBlockingStreamReader:
+    def __init__(self, stream):
+        self._s = stream
+        self._q = Queue()
+
+        def _populateQueue(stream, queue):
+            while True:
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+                else:
+                    sys.exit(0)
+
+        self._t = threading.Thread(target = _populateQueue, args = (self._s, self._q))
+        self._t.daemon = True
+        self._t.start()
+
+    def readline(self, timeout = None):
+        try:
+            return self._q.get(block = timeout is not None, timeout = timeout)
+        except Empty:
+            return None
+
+class UnexpectedEndOfStream(Exception): pass
 
 args = []
 if '--player' in sys.argv:
@@ -64,10 +90,33 @@ def wf(stream, text):
 
 ITC = {'recording': False, 'exit': exit, 'key': key, 'playing': True, 'begin': .0, 'end': .0}
 
-def stderr_handle(player, itc):
+def stderr_handle(player):
+	tmp = 'dummy'
+	nbsr = NonBlockingStreamReader(player.stderr)
 	while True:
-		tmp = player.stderr.readline()
-		if tmp == 'No bind found for key \'' + itc['key'] + '\'.\n':
+		try:
+			tmp = nbsr.readline(0.1)
+		except UnexpectedEndOfStream:
+			yield '*quit'
+		yield tmp
+
+def stdout_handle(player):
+	tmp = 'dummy'
+	nbsr = NonBlockingStreamReader(player.stdout)
+	while True:
+		tmp = nbsr.readline(0.1)
+		yield tmp
+
+def worker(player, itc):
+	outputs = [stderr_handle(player), stdout_handle(player)]
+	turn = 1
+	while True:
+		turn = 0 if turn != 0 else 1
+		output = outputs[turn].next()
+		if not output: continue
+		if output == '*quit': return
+
+		if output.find('No bind found for key \'' + itc['key'] + '\'.\n') != -1:
 			if not itc['recording']:
 				print '\n[gee] gif recording started'
 				itc['recording'] = True
@@ -82,27 +131,17 @@ def stderr_handle(player, itc):
 				wf(player.stdin, 'osd 1')
 				wf(player.stdin, 'osd_show_text "GIF recording ended, saving..." 1000 1')
 				wf(player.stdin, 'get_time_pos')
-		else:
-			if tmp.replace('\n', '') != '':
-				sys.stderr.write(tmp)
-				sys.stderr.flush()
-
-		if tmp == '':
-			itc['playing'] = False
-			break
-
-def stdout_handle(player, itc):
-	while True:
-		tmp = player.stdout.readline()
-		if tmp.replace('\n', '').startswith('ANS_TIME_POSITION'):
+			continue
+			
+		if output.replace('\n', '').startswith('ANS_TIME_POSITION'):
 			if itc['recording']:
-				itc['begin'] = float(tmp.split('=')[1].replace('\n', ''))
+				itc['begin'] = float(output.split('=')[1].replace('\n', ''))
 			else:
-				itc['end'] = float(tmp.split('=')[1].replace('\n', ''))
+				itc['end'] = float(output.split('=')[1].replace('\n', ''))
 				print '[gee] total gif time: ' + str(itc['end'] - itc['begin']) + 's (' + str(itc['end']) + ' - ' + str(itc['begin']) + ')'
 				wf(player.stdin, 'get_file_name')
-		elif tmp.replace('\n', '').startswith('ANS_FILENAME'):
-			filename = eval(tmp.split('=')[1])
+		elif output.replace('\n', '').startswith('ANS_FILENAME'):
+			filename = eval(output.split('=')[1])
 			saveto = str(time.time()) + '.gif'
 
 			#here we record
@@ -110,16 +149,8 @@ def stdout_handle(player, itc):
 			wf(player.stdin, 'osd 1')
 			wf(player.stdin, 'osd_show_text "Saved to \'' + saveto + '\'" 2000 1')
 		else:
-			if not tmp.startswith('A: '):
-				sys.stdout.write(tmp)
+			if not output.startswith('A: '):
+				sys.stdout.write(output)
 				sys.stdout.flush()
 
-		if tmp == '':
-			break #stderr handler is the one who tells main thread to quit
-
-threading.Thread(target=stderr_handle, args=(player, ITC,)).start()
-threading.Thread(target=stdout_handle, args=(player, ITC,)).start()
-
-while ITC['playing']:
-	time.sleep(1)
-exit()
+worker(player, ITC)
